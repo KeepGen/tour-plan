20cce80121667511e5cc523205dedb5a6cbcb5fb
KeepGen <keeper.gen@gmail.com>
2020-07-22 00:10:59 +0300

    create: added yandex maps in hotel section js api

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ var mySwiper = new Swiper('.swiper-container', {
     enabled: true,
   },
 });
+
+ymaps.ready(init);
+
+function init() {
+
+  var myMap = new ymaps.Map("map", {
+    center: [7.890703, 98.294204],
+    zoom: 16,
+  });
+
+  // Метка с текстом
+  var placemark3 = new ymaps.Placemark([7.890703, 98.294709], {
+    iconContent: '<strong>DoubleTree</strong> by Hilton Phuket'
+  }, {
+    'preset': 'islands#yellowStretchyIcon'
+  });
+
+  myMap.geoObjects.add(placemark3);
+}
