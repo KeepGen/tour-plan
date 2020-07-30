9799f43ef5206c353aa9f37479d474f694719304
KeepGen <keeper.gen@gmail.com>
2020-07-30 12:22:48 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -35,6 +35,13 @@ $('.parallax-window').parallax({
   imageSrc: 'img/newsletter-bg.jpg'
 });
 
+// Подключение функции активации мобильного меню
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function () {
+  console.log('Клик по кнопке меню');
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom_visible');
+});
+
 // Подключение Карты Яндекс по API
 ymaps.ready(init);
 
