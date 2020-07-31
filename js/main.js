f98b49fe2d426c22f49eaf1512b9b0e66785e56c
KeepGen <keeper.gen@gmail.com>
2020-07-31 19:25:02 +0300

    create: added modal window for booking +responsive

--- js/main.js
+++ js/main.js
@@ -1,61 +1,93 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  speed: 800,
-  effect: 'slide',
+$(document).ready(function () {
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-})
-// Keyboard navigation
-var hotelSlider = new Swiper('.hotel-slider', {
-  keyboard: {
-    enabled: true,
-  },
-});
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    speed: 800,
+    effect: 'slide',
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  speed: 800,
-  effect: 'slide',
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-
-// Покдлючение Параллакс
-$('.parallax-window').parallax({
-  imageSrc: 'img/newsletter-bg.jpg'
-});
-
-// Подключение функции активации мобильного меню
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке меню');
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom_visible');
-});
-
-// Подключение Карты Яндекс по API
-ymaps.ready(init);
-
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [7.890996, 98.293066],
-    zoom: 16,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+  // Keyboard navigation
+  var hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
+      enabled: true,
+    },
   });
 
-  // Метка с текстом
-  var placemark3 = new ymaps.Placemark([7.890703, 98.294709], {
-    iconContent: '<strong>DoubleTree</strong> by Hilton Phuket'
-  }, {
-    'preset': 'islands#redStretchyIcon'
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    speed: 800,
+    effect: 'slide',
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
   });
-  myMap.geoObjects.add(placemark3);
-}
+
+
+  /*   // Покдлючение Параллакс
+    $('.parallax-window').parallax({
+      imageSrc: 'img/newsletter-bg.jpg'
+    }); */
+
+
+  // Подключение функции активации мобильного меню
+  var menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    $(".navbar-bottom").toggleClass('navbar-bottom_visible');
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay_visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog_visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay_visible");
+    modalDialog.removeClass("modal__dialog_visible");
+  }
+
+  $(document).keyup(function (event) {
+    if (event.which == '27') {
+      $('.modal__overlay').removeClass('modal__overlay_visible');
+      $('.modal__dialog').removeClass('modal__dialog_visible');
+    }
+  });
+
+
+  // Подключение Карты Яндекс по API
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.890996, 98.293066],
+      zoom: 16,
+    });
+
+    // Метка с текстом
+    var placemark3 = new ymaps.Placemark([7.890703, 98.294709], {
+      iconContent: '<strong>DoubleTree</strong> by Hilton Phuket'
+    }, {
+      'preset': 'islands#redStretchyIcon'
+    });
+    myMap.geoObjects.add(placemark3);
+  }
+
+});
